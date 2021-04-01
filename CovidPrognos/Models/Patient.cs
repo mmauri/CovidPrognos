cefb29aca087130cf95406c9fe864997b4b26790
Marc Mauri <marc@kaizenforpharma.com>
2021-04-01 11:37:06 +0200

    Add project files.

---
+++ CovidPrognos/Models/Patient.cs
@@ -0,0 +1,23 @@
+﻿namespace CovidPrognos.Models
+{
+    public class Patient
+    {
+        public string PatientId { get; set; }
+        public System.DateTime AdmissionDate { get; set; }
+        public double Age { get; set; }
+        public bool IsMale { get; set; }
+        public bool SaO2Below90PercAtAdmission { get; set; }
+        public bool CRPGreater112mglAtAdmission { get; set; } //Proteine C reactive level mg/L
+        public double Neutrophils { get; set; }
+        public double Platelets { get; set; }
+        public double NPRScore { get; set; }
+    }
+    public class PatientData
+    {
+        public string PatientId { get; set; }
+        public int DayFromAdmission { get; set; }
+        
+        
+        
+    }
+}
---
+++ CovidPrognos/Program.cs
@@ -0,0 +1,28 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Radzen;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CovidPrognos
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddScoped<DialogService>();
+            builder.Services.AddScoped<NotificationService>();
+            builder.Services.AddScoped<TooltipService>();
+            //builder.Services.AddScoped<ContextMenuService>();
+            builder.Services.AddBlazoredLocalStorage();
+            await builder.Build().RunAsync();
+        }
+    }
+}
